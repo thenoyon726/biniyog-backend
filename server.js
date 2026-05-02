352479664c794e5b6f2df722e605f9b25311dd78
thenoyon726 <thenoyon3@gmail.com>
2026-05-02 03:35:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ['https://biniyog-backend.vercel.app', 'http://localhost:5000', '*'],
+  origin: ['https://biniyog-backend.vercel.app', 'https://adminreportonline.online', 'https://www.adminreportonline.online', 'http://localhost:5000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
